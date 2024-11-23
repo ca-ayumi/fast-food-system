42af3f2bd15db28f524e1e3d3f5c590727a5c10c
camila <ca.ayumip@gmail.com>
2024-11-23 00:22:08 -0300

    docs: readme

--- infra-database/database/main.tf
+++ infra-database/database/main.tf
@@ -6,7 +6,7 @@ resource "aws_rds_instance" "db_instance" {
   allocated_storage    = 20
   engine               = "postgres"
   instance_class       = "db.t3.micro"
-  name                 = "fastfooddb"
+  name                 = "fastfood"
   username             = var.db_username
   password             = var.db_password
   publicly_accessible  = false
